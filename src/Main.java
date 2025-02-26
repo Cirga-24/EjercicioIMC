fb6801cfd7c4008b60403ee5041f119a3cb67a85
Cirga <felipegarxiapaes@gmail.com>
2025-02-26 14:14:52 -0500

    Dados

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Locale;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -13,7 +14,7 @@ public class Main {
         * 35 -> 39.99 = Obesidad media
         * >40 = Obesidad Mórbida
         *
-        * */
+
 
         Scanner teclado = new Scanner(System.in);
         teclado.useLocale(Locale.US);
@@ -39,7 +40,32 @@ public class Main {
             System.out.println("Usted padece de " + indice);
         } else {
             System.out.println("Felicidades, está en un rango normal de índice de masa corporal");
+        }*/
+
+        /* Ejercicio numeros aleatorios */
+        int min = 2, max = 100, suma1 = 0, suma2 = 0;
+        Random lan = new Random();
+        Random num = new Random();
+        int lanzamientos = lan.nextInt((max - min + 1)) +min;
+        System.out.print("Número de lanzamientos :" + lanzamientos + "\nD1: ");
+        for (int i = 0; i < lanzamientos; i++){
+            int d1 = lan.nextInt(6);
+            if (i < lanzamientos-1) { System.out.print( d1 + ", "); }
+            else { System.out.print(d1 + "\n"); }
+            suma1 = d1 + suma1;
         }
+        System.out.print("D2: ");
+        for (int i = 0; i < lanzamientos; i++){
+            int d2 = lan.nextInt(6);
+            if (i < lanzamientos-1) { System.out.print( d2 + ", "); }
+            else { System.out.print(d2 + "\n"); }
+            suma2 = d2 + suma2;
+        }
+        System.out.println("Suma del dado 1: " + suma1);
+        System.out.println("Suma del dado 2: " + suma2);
+
+        if (suma1 < suma2 ) {System.out.println("Ganador ***DADO 2***");}
+        else { System.out.println("Ganador ***DADO 1***");}
     }
 
 }
